83ee6876a9df091173a4543a313b07218bb7ce29
chinazes532 <chernikof50@gmail.com>
2025-04-14 19:44:25 +0300

    Добавил клавиатуру для пользователя, которая выводит все категории

--- app/keyboards/builder.py
+++ app/keyboards/builder.py
@@ -1,2 +1,15 @@
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from app.database.requests.vpn_categories.select import get_vpn_categories
+
+
+async def user_vpn_categories():
+    kb = InlineKeyboardBuilder()
+
+    categories = await get_vpn_categories()
+
+    for category in categories:
+        kb.row(InlineKeyboardButton(text=category.name, callback_data=f"category_{category.id}"))
+
+    return kb.as_markup()
