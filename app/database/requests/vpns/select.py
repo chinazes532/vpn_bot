e126e6f9a13fedac8c5978fe4f361c5675451afc
chinazes532 <chernikof50@gmail.com>
2025-04-14 20:02:10 +0300

    Исправил ошибку со scalarS

--- app/database/requests/vpns/select.py
+++ app/database/requests/vpns/select.py
@@ -7,3 +7,9 @@ async def get_vpn_by_category_id(category_id):
     async with async_session() as session:
         vpns = await session.scalars(select(VPN).where(VPN.category_id == category_id))
         return vpns
+
+
+async def get_vpn_by_id(vpn_id):
+    async with async_session() as session:
+        vpn = await session.scalar(select(VPN).where(VPN.id == vpn_id))
+        return vpn
