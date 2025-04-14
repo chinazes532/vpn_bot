a800699bb2069a45fd33e235035424aaf1220860
chinazes532 <chernikof50@gmail.com>
2025-04-14 19:52:02 +0300

    Добавил клавиатуру на просмотр впнов по категории

--- app/keyboards/builder.py
+++ app/keyboards/builder.py
@@ -2,6 +2,7 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from app.database.requests.vpn_categories.select import get_vpn_categories
+from app.database.requests.vpns.select import get_vpn_by_category_id
 
 
 async def user_vpn_categories():
@@ -11,5 +12,16 @@ async def user_vpn_categories():
 
     for category in categories:
         kb.row(InlineKeyboardButton(text=category.name, callback_data=f"category_{category.id}"))
+
+    return kb.as_markup()
+
+
+async def user_countries(vpn_category_id):
+    kb = InlineKeyboardBuilder()
+
+    countries = await get_vpn_by_category_id(vpn_category_id)
+
+    for country in countries:
+        kb.row(InlineKeyboardButton(text=country.name, callback_data=f"country_{country.id}"))
 
     return kb.as_markup()
